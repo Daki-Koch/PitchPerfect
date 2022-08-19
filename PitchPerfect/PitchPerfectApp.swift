42a63eec09bf5e92a2a4619dc5c3ed60e5f64899
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-08-19 16:14:37 +0200

    Initial Commit

---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by David Koch on 19.08.22.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
